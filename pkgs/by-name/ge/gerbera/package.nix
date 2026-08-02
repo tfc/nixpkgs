536843956277214f02ce03bf8407d4050c332b08
Jacek Galowicz <jacek@applicative.systems>
2026-08-02 17:30:49 +0000

    gerbera: simplify with lib.cmakeBool

--- pkgs/by-name/ge/gerbera/package.nix
+++ pkgs/by-name/ge/gerbera/package.nix
@@ -149,7 +149,7 @@ stdenv.mkDerivation (finalAttrs: {
     # systemd service will be generated alongside the service
     "-DWITH_SYSTEMD=OFF"
   ]
-  ++ map (e: "-DWITH_${e.name}=${if e.enable then "ON" else "OFF"}") options;
+  ++ map (e: lib.cmakeBool "WITH_${e.name}" e.enable) options;
 
   nativeBuildInputs = [
     cmake
