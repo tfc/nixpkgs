b9e8683096c63fec504a3b841b67e64f399a3be5
Jacek Galowicz <jacek@applicative.systems>
2026-08-04 15:15:52 +0200

    abseil-cpp_202103: simplify with lib.cmakeBool

--- pkgs/development/libraries/abseil-cpp/202103.nix
+++ pkgs/development/libraries/abseil-cpp/202103.nix
@@ -49,7 +49,7 @@ stdenv.mkDerivation rec {
   ];
 
   cmakeFlags = [
-    "-DBUILD_SHARED_LIBS=${if static then "OFF" else "ON"}"
+    (lib.cmakeBool "BUILD_SHARED_LIBS" (!static))
   ]
   ++ lib.optionals (cxxStandard != null) [
     "-DCMAKE_CXX_STANDARD=${cxxStandard}"
