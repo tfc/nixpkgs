226439831944ce0c3ecfea68a9ab67227ef872d9
Jacek Galowicz <jacek@applicative.systems>
2026-08-04 15:15:58 +0200

    hepmc3: simplify with lib.cmakeBool

--- pkgs/development/libraries/physics/hepmc3/default.nix
+++ pkgs/development/libraries/physics/hepmc3/default.nix
@@ -44,7 +44,7 @@ stdenv.mkDerivation rec {
 
   cmakeFlags = [
     "-DHEPMC3_CXX_STANDARD=17"
-    "-DHEPMC3_ENABLE_PYTHON=${if withPython then "ON" else "OFF"}"
+    (lib.cmakeBool "HEPMC3_ENABLE_PYTHON" withPython)
   ]
   ++ lib.optionals withPython [
     "-DHEPMC3_PYTHON_VERSIONS=${if python.isPy3k then "3.X" else "2.X"}"
