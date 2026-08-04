84ff0b118c836d1e0ae9d61326ec4268caad684f
Jacek Galowicz <jacek@applicative.systems>
2026-08-04 15:15:54 +0200

    catch2_3: simplify with lib.cmakeBool

--- pkgs/by-name/ca/catch2_3/package.nix
+++ pkgs/by-name/ca/catch2_3/package.nix
@@ -37,7 +37,7 @@ stdenv.mkDerivation rec {
 
   cmakeFlags = [
     "-DCATCH_DEVELOPMENT_BUILD=ON"
-    "-DCATCH_BUILD_TESTING=${if doCheck then "ON" else "OFF"}"
+    (lib.cmakeBool "CATCH_BUILD_TESTING" doCheck)
     "-DCATCH_ENABLE_WERROR=OFF"
   ]
   ++ lib.optionals (stdenv.cc.isClang && doCheck) [
